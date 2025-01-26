fd02ed3d1d33b7bd2afcf89ceddbb89a7683bcfa
gayashan4lk <gayashan4lk@gmail.com>
2025-01-27 01:33:28 +0530

    change input to text box

--- lib/components/GenerateText.tsx
+++ lib/components/GenerateText.tsx
@@ -4,13 +4,17 @@ import { useState } from 'react'
 import useSWRMutation from 'swr/mutation'
 
 export function GenerateText() {
-  const fetchChat = (url: string, { arg }: { arg: string }) => fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ userMessage: arg }),
-  }).then((res) => res.json())
+  const fetchChat = (url: string, { arg }: { arg: string }) =>
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userMessage: arg }),
+    }).then((res) => res.json())
 
-  const { trigger, data, error, isMutating } = useSWRMutation('/api/chat', fetchChat)
+  const { trigger, data, error, isMutating } = useSWRMutation(
+    '/api/chat',
+    fetchChat
+  )
   const [inputText, setInputText] = useState('')
 
   if (isMutating) return <div>Loading...</div>
@@ -19,19 +23,33 @@ export function GenerateText() {
   return (
     <div>
       {data && (
-        <div className="p-3 my-2 rounded-lg bg-white shadow-sm text-gray-700">
+        <div className="p-3 my-2 rounded-lg bg-white shadow-sm text-gray-700 w-full">
           <p className="text-xs opacity-60">{data.message.role}</p>
           <p>{data.message.content}</p>
         </div>
       )}
 
-      <form onSubmit={(e) => { e.preventDefault(); trigger(inputText) }}>
-        <input
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          trigger(inputText)
+        }}
+      >
+        {/* <input
           className='py-1 px-2 border-2 rounded-md'
           type="text"
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
-        />
+        /> */}
+        <div>
+        <textarea
+          name=""
+          id=""
+          className="py-1 px-2 border-2 rounded-md w-full"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        ></textarea>
+        </div>
         <button
           className="py-1 px-2 bg-violet-500 hover:bg-violet-700 text-white font-bold rounded-md"
           type="submit"
@@ -39,7 +57,6 @@ export function GenerateText() {
           Generate
         </button>
       </form>
-
     </div>
   )
 }
